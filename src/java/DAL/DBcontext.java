84e15f89ec229eb5f3e190002bda63ff40436d04
Ngochung1772k4 <ngochung1772k4@gmail.com>
2025-02-20 22:59:43 +0700

    V6

--- src/java/DAL/DBcontext.java
+++ src/java/DAL/DBcontext.java
@@ -4,15 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBcontext {
+
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=hungdepzai"; 
-            String user = "hung"; 
-            String password = "123";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=hungdepzai";
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Kết nối thành công!"); 
+            System.out.println("Kết nối thành công!");
         } catch (Exception e) {
             e.printStackTrace();
         }
